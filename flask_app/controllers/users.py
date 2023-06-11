67d647337d23349c75cebe69a8c0fb9837dd3dfd
Jon Griggs <bigmac_beats1773@outlook.com>
2023-06-11 13:36:23 -0500

    finished log/reg form

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,4 +1,5 @@
 from flask_app import app
+from flask_app.models.user import User
 from flask import render_template, redirect, request, session, flash
 
 @app.route('/')
@@ -8,3 +9,16 @@ def register_page():
 @app.route('/login')
 def login_page():
     return render_template("login.html")
+
+@app.route('/confirm/register', methods = ['POST'])
+def sumbit_registration():
+    if User.create_user(request.form):
+        return redirect('/dashboard')
+    else:
+        return redirect('/')
+    
+
+#! Add appropriate variables to dashboard
+@app.route('/dashboard')
+def render_dashboard():
+    return render_template("dashboard.html")
